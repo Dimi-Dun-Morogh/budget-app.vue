2c1c6dd987a52e82fdf8ab9cd6372d900bf112ee
Dimi-Dun-Morogh <darnelbro@gmail.com>
2020-04-21 19:24:20 +0300

    placed BudgetlistItem in separate component

--- src/components/BudgetList.vue
+++ src/components/BudgetList.vue
@@ -2,11 +2,12 @@
   <div class="budget-list-wrap">
     <ElCard :header="header">
       <template v-if="!isEmpty">
-<div class="list-item" v-for="(item, prop) in list" :key="prop">
+<BudgetListItem :list="list" @deleteItem="deleteItem"/>
+<!-- <div class="list-item" v-for="(item, prop) in list" :key="prop">
   <span  class="budget-comment"> {{  item.comment}}</span>
    <span  class="budget-value"> {{  item.value}}</span>
    <ElButton type="danger" size="mini" @click="deleteItem(item.id)">Delete</ElButton>
-</div>
+</div> -->
          </template>
 <ElAlert v-else type="info" :title="emptyTitle" :closable="false">  </ElAlert>
 
@@ -15,8 +16,13 @@
 </template>
 
 <script>
+import BudgetListItem from '@/components/BudgetListItem.vue';
+
 export default {
   name: 'BudgetList',
+  components: {
+    BudgetListItem,
+  },
   props: {
     list: {
       type: Object,
@@ -45,15 +51,4 @@ export default {
   max-width: 500px;
   margin: auto;
 }
-.list-item{
-  display: flex;
-  align-items: center;
-  padding: 10px 15px;
-}
-.budget-value {
-  font-weight: bold;
-  margin-left: auto;
-  margin-right: 20px;
-}
-
 </style>
---
+++ src/components/BudgetListItem.vue
@@ -0,0 +1,34 @@
+<template>
+<div>
+<div class="list-item" v-for="(item, prop) in list" :key="prop">
+  <span  class="budget-comment"> {{  item.comment}}</span>
+   <span  class="budget-value"> {{  item.value}}</span>
+   <ElButton type="danger" size="mini" @click="deleteItem(item.id)">Delete</ElButton>
+</div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'BudgetListItem',
+  props: ['list'],
+  methods: {
+    deleteItem(id) {
+      this.$emit('deleteItem', id);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.budget-value {
+  font-weight: bold;
+  margin-left: auto;
+  margin-right: 20px;
+}
+.list-item{
+  display: flex;
+  align-items: center;
+  padding: 10px 15px;
+}
+</style>
